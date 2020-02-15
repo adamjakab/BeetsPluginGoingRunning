7d39771af7f4691fa4e03929dbda40f78c5d8a31
Adam Jakab <jakabadambalazs@gmail.com>
2020-02-15 11:11:39 +0100

    plugin subcommand working

--- beetsplug/goingrunning.py
+++ beetsplug/goingrunning.py
@@ -1,6 +1,16 @@
 from beets.plugins import BeetsPlugin
+from beets.ui import Subcommand
 
 
-class GoingrunningPlugin(BeetsPlugin):
-    pass
+def command_body(lib, opts, args):
+    print("Are you going running?")
+
+
+going_running_command = Subcommand('goingrunning', help='bring some music with you that matches your training')
+going_running_command.func = command_body
+
+
+class GoingRunningPlugin(BeetsPlugin):
+    def commands(self):
+        return [going_running_command]
 
