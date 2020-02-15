773fe1d96b0caad4b355f8aabece234bc5d2da1b
Adam Jakab <jakabadambalazs@gmail.com>
2020-02-15 17:05:53 +0100

    accessing configuration

--- beetsplug/goingrunning.py
+++ beetsplug/goingrunning.py
@@ -11,7 +11,8 @@ import logging
 from optparse import OptionParser
 
 # import beets
-from beets import importer, config
+from beets import config as beets_global_config
+from beets.library import Library as BeatsLibrary
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand, decargs, colorize, input_yn, UserError
 from beets.library import ReadError
@@ -19,6 +20,10 @@ from beets.util import cpu_count, displayable_path, syspath
 
 # Module methods
 log = logging.getLogger('beets.check')
+
+
+def get_beets_global_config():
+    return beets_global_config.flatten()
 
 
 # Classes ###
@@ -51,12 +56,14 @@ class GoingRunningPlugin(BeetsPlugin):
 
 
 class GoingRunningCommand(Subcommand):
+    config = None
     quiet = False
     lib = None
     query = None
     parser = None
 
-    def __init__(self, config):
+    def __init__(self, cfg):
+        self.config = cfg.flatten()
         # self.threads = config['threads'].get(int)
         # self.check_integrity = config['integrity'].get(bool)
 
@@ -81,7 +88,7 @@ class GoingRunningCommand(Subcommand):
             help=u'bring some music with you that matches your training'
         )
 
-    def func(self, lib, options, arguments):
+    def func(self, lib: BeatsLibrary, options, arguments):
         self.quiet = options.quiet
         self.lib = lib
         arguments = decargs(arguments)
@@ -98,11 +105,38 @@ class GoingRunningCommand(Subcommand):
         else:
             self.handle_training()
 
+    # @todo: explain keys
+    # @todo: order keys
     def list_trainings(self):
-        log.debug("Listing trainings...")
+        self._say("List trainings:")
+        training_names = list(self.config["trainings"].keys())
+        for training_name in training_names:
+            self.list_training_attributes(training_name)
+
+    def list_training_attributes(self, training_name):
+        self._say("{0} ::: {1}".format("=" * 40, training_name))
+        training = self.config["trainings"].get(training_name)
+        tkeys = training.keys()
+        for tkey in tkeys:
+            tval = training.get(tkey)
+            self._say("{0} : {1}".format(tkey, tval))
 
     def handle_training(self):
-        training = self.query[0]
-        log.debug("Handling training: {}".format(training))
+        training_name = self.query[0]
+        training = self.config["trainings"].get(training_name)
+        if not training:
+            log.warning("There is no training registered with this name!")
+            return
+
+        self._say("Handling training: {}".format(training_name))
+        self.list_training_attributes(training_name)
+
+        i = self.lib.items()
+        print(i)
 
 
+    def _say(self, msg):
+        if not self.quiet:
+            log.info(msg)
+        else:
+            log.debug(msg)
