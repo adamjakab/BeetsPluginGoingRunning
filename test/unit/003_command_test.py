671bc7aebd827a071631debecee28524056b94c2
Adam Jakab <jakabadambalazs@gmail.com>
2020-04-05 00:09:28 +0200

    more query parsing tests

--- test/unit/003_command_test.py
+++ test/unit/003_command_test.py
@@ -1,7 +1,7 @@
 #  Copyright: Copyright (c) 2020., Adam Jakab
 #  Author: Adam Jakab <adam at jakab dot pro>
 #  License: See LICENSE.txt
-
+from beets.library import Item
 from beets.util.confit import Subview
 from beetsplug.goingrunning import GoingRunningCommand
 from beetsplug.goingrunning import command
@@ -27,14 +27,18 @@ class CommandTest(UnitTestHelper):
         inst = command.GoingRunningCommand(config)
         self.assertEqual(config, inst.config)
 
-    def test_gather_query_elements__test_1(self):
+    def test_gather_parse_query_elements__test_1(self):
         plg_cfg: Subview = self.config["goingrunning"]
         training: Subview = plg_cfg["trainings"]["q-test-1"]
         cmd = GoingRunningCommand(plg_cfg)
         elements = cmd._gather_query_elements(training)
         self.assertListEqual([], elements)
 
-    def test_gather_query_elements__test_2(self):
+        query = cmd.parse_query_elements(elements, Item)
+        expected = "AndQuery([TrueQuery()])"
+        self.assertEqual(expected, str(query))
+
+    def test_gather_parse_query_elements__test_2(self):
         plg_cfg: Subview = self.config["goingrunning"]
         training: Subview = plg_cfg["trainings"]["q-test-2"]
         cmd = GoingRunningCommand(plg_cfg)
@@ -42,7 +46,17 @@ class CommandTest(UnitTestHelper):
         expected = ['bpm:100..150', 'length:120..300', 'genre:hard rock']
         self.assertListEqual(expected, elements)
 
-    def test_gather_query_elements__test_3(self):
+        query = cmd.parse_query_elements(elements, Item)
+        expected = (
+            "AndQuery(["
+            "NumericQuery('bpm', '100..150', True), "
+            "DurationQuery('length', '120..300', True), "
+            "SubstringQuery('genre', 'hard rock', True)"
+            "])"
+        )
+        self.assertEqual(expected, str(query))
+
+    def test_gather_parse_query_elements__test_3(self):
         plg_cfg: Subview = self.config["goingrunning"]
         training: Subview = plg_cfg["trainings"]["q-test-3"]
         cmd = GoingRunningCommand(plg_cfg)
@@ -50,7 +64,17 @@ class CommandTest(UnitTestHelper):
         expected = ['bpm:100..150', 'length:120..300', 'genre:reggae']
         self.assertListEqual(expected, elements)
 
-    def test_gather_query_elements__test_4(self):
+        query = cmd.parse_query_elements(elements, Item)
+        expected = (
+            "AndQuery(["
+            "NumericQuery('bpm', '100..150', True), "
+            "DurationQuery('length', '120..300', True), "
+            "SubstringQuery('genre', 'reggae', True)"
+            "])"
+        )
+        self.assertEqual(expected, str(query))
+
+    def test_gather_parse_query_elements__test_4(self):
         plg_cfg: Subview = self.config["goingrunning"]
         training: Subview = plg_cfg["trainings"]["q-test-4"]
         cmd = GoingRunningCommand(plg_cfg)
@@ -59,7 +83,20 @@ class CommandTest(UnitTestHelper):
                     'genre:reggae', 'year:1960..1969']
         self.assertListEqual(expected, elements)
 
-    def test_gather_query_elements__test_4_bis(self):
+        query = cmd.parse_query_elements(elements, Item)
+        expected = (
+            "AndQuery(["
+            "NumericQuery('bpm', '100..150', True), "
+            "OrQuery(["
+            "NumericQuery('year', '2015..', True), "
+            "NumericQuery('year', '1960..1969', True)"
+            "]), "
+            "SubstringQuery('genre', 'reggae', True)"
+            "])"
+        )
+        self.assertEqual(expected, str(query))
+
+    def test_gather_parse_query_elements__test_4_bis(self):
         """Command line query should always be the first in the list
         """
         plg_cfg: Subview = self.config["goingrunning"]
@@ -72,7 +109,21 @@ class CommandTest(UnitTestHelper):
                     'genre:reggae', 'year:1960..1969']
         self.assertListEqual(expected, elements)
 
-    def test_gather_query_elements__test_5(self):
+        query = cmd.parse_query_elements(elements, Item)
+        expected = (
+            "AndQuery(["
+            "SubstringQuery('albumartist', 'various artists', True), "
+            "NumericQuery('bpm', '100..150', True), "
+            "OrQuery(["
+            "NumericQuery('year', '2015..', True), "
+            "NumericQuery('year', '1960..1969', True)"
+            "]), "
+            "SubstringQuery('genre', 'reggae', True)"
+            "])"
+        )
+        self.assertEqual(expected, str(query))
+
+    def test_gather_parse_query_elements__test_5(self):
         plg_cfg: Subview = self.config["goingrunning"]
         training: Subview = plg_cfg["trainings"]["q-test-5"]
         cmd = GoingRunningCommand(plg_cfg)
@@ -80,3 +131,18 @@ class CommandTest(UnitTestHelper):
         expected = ['genre:rock', 'genre:blues', 'genre:ska',
                     'genre:funk', 'genre:Rockabilly', 'genre:Disco']
         self.assertListEqual(expected, elements)
+
+        query = cmd.parse_query_elements(elements, Item)
+        expected = (
+            "AndQuery(["
+            "OrQuery(["
+            "SubstringQuery('genre', 'rock', True), "
+            "SubstringQuery('genre', 'blues', True), "
+            "SubstringQuery('genre', 'ska', True), "
+            "SubstringQuery('genre', 'funk', True), "
+            "SubstringQuery('genre', 'Rockabilly', True), "
+            "SubstringQuery('genre', 'Disco', True)"
+            "])"
+            "])"
+        )
+        self.assertEqual(expected, str(query))
