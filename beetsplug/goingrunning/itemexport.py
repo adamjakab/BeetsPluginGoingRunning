4bcfc08a190d8e614ef8ac7ce4d448b3f6d0ce54
Adam Jakab <jakabadambalazs@gmail.com>
2020-04-06 17:02:51 +0200

    Fix #7 - added progress bar when copying to target

--- beetsplug/goingrunning/itemexport.py
+++ beetsplug/goingrunning/itemexport.py
@@ -5,9 +5,11 @@ import os
 import random
 import string
 import tempfile
+from datetime import datetime
 from glob import glob
 from pathlib import Path
 
+from alive_progress import alive_bar
 from beets import util
 from beets.util.confit import Subview
 from beetsplug.goingrunning import common
@@ -44,26 +46,27 @@ class ItemExport:
             return
 
         dst_path = common.get_destination_path_for_training(self.training)
-        playlist_filename = "playlist.m3u"
+        training_name = str(self.training.name).split(".").pop()
+        playlist_filename = "{}.m3u".format(training_name)
         dst = "{0}/{1}".format(dst_path, playlist_filename)
 
-        lines = []
-        header = "# Playlist created for `goingrunning` training: {}\n". \
-            format(self.training.name)
-        lines.append(header)
+        lines = [
+            "# Playlist generated for training '{}' on {}". \
+                format(training_name, datetime.now())
+        ]
 
         for item in self.items:
             path = util.displayable_path(item.get("exportpath",
                                                   item.get("path")))
             if path:
                 path = util.syspath(path)
-                line = "{path}\n".format(path=path)
+                line = "{path}".format(path=path)
                 lines.append(line)
 
         with tempfile.NamedTemporaryFile(mode='w+b', delete=False) as ntf:
             tmp_playlist = ntf.name
             for line in lines:
-                ntf.write(line.encode("utf-8"))
+                ntf.write("{}\n".format(line).encode("utf-8"))
 
         common.say("Created playlist: {0}".format(dst), log_only=True)
         util.copy(tmp_playlist, dst)
@@ -75,14 +78,14 @@ class ItemExport:
         if not common.get_target_attribute_for_training(self.training,
                                                         "copy_files"):
             common.say("Copying to target[{0}] was skipped (copy_files=no).".
-                       format(target_name), log_only=False)
+                       format(target_name))
             return
 
         increment_play_count = common.get_training_attribute(
             self.training, "increment_play_count")
         dst_path = common.get_destination_path_for_training(self.training)
         common.say("Copying to target[{0}]: {1}".
-                   format(target_name, dst_path), log_only=False)
+                   format(target_name, dst_path))
 
         # todo: move to common
         def random_string(length=6):
@@ -90,30 +93,32 @@ class ItemExport:
             return ''.join(random.choice(letters) for i in range(length))
 
         cnt = 0
-        for item in self.items:
-            src = util.displayable_path(item.get("path"))
-            if not os.path.isfile(src):
-                # todo: this is bad enough to interrupt! create option for this
-                common.say("File does not exist: {}".format(src))
-                continue
+        with alive_bar(len(self.items)) as bar:
+            for item in self.items:
+                src = util.displayable_path(item.get("path"))
+                if not os.path.isfile(src):
+                    # todo: this is bad enough to interrupt! create option
+                    #  for this
+                    common.say("File does not exist: {}".format(src))
+                    continue
 
-            fn, ext = os.path.splitext(src)
-            gen_filename = "{0}_{1}{2}".format(str(cnt).zfill(6),
-                                               random_string(), ext)
-            dst = "{0}/{1}".format(dst_path, gen_filename)
-            common.say("Copying[{1}]: {0}".format(src, gen_filename),
-                       log_only=True)
+                fn, ext = os.path.splitext(src)
+                gen_filename = "{0}_{1}{2}".format(str(cnt).zfill(6),
+                                                   random_string(), ext)
+                dst = "{0}/{1}".format(dst_path, gen_filename)
+                common.say("Copying[{1}]: {0}".format(src, gen_filename))
 
-            if not self.cfg_dry_run:
-                util.copy(src, dst)
+                if not self.cfg_dry_run:
+                    util.copy(src, dst)
 
-                # store the file_name for the playlist
-                item["exportpath"] = util.bytestring_path(gen_filename)
+                    # store the file_name for the playlist
+                    item["exportpath"] = util.bytestring_path(gen_filename)
 
-                if increment_play_count:
-                    common.increment_play_count_on_item(item)
+                    if increment_play_count:
+                        common.increment_play_count_on_item(item)
 
-            cnt += 1
+                cnt += 1
+                bar()
 
     def _clean_target(self):
         target_name = common.get_training_attribute(self.training, "target")
@@ -123,7 +128,7 @@ class ItemExport:
             dst_path = common.get_destination_path_for_training(self.training)
 
             common.say("Cleaning target[{0}]: {1}".
-                       format(target_name, dst_path), log_only=False)
+                       format(target_name, dst_path))
             song_extensions = ["mp3", "mp4", "flac", "wav", "ogg", "wma", "m3u"]
             target_file_list = []
             for ext in song_extensions:
@@ -144,7 +149,7 @@ class ItemExport:
             root = Path(root).expanduser()
 
             common.say("Deleting additional files: {0}".
-                       format(additional_files), log_only=False)
+                       format(additional_files))
 
             for path in additional_files:
                 path = Path(str.strip(path, "/"))
